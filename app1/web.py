1b03c2b202a20af68a1a3089635b521ce5bca216
INDRANEEL8055 <codelearning8055@gmail.com>
2024-01-26 12:00:45 +0530

    day19 of developing webapp:- we used streamlit to create a webapp for todo and added the add functionality with the help of session_state

--- app1/web.py
+++ app1/web.py
@@ -3,13 +3,21 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n" # session_state contains pari of data which user enters
+    todos.append(todo)
+    functions.write_todos(todos)
 
-st.title("My To-Do App")
-st.subheader("This is my todoapp")
-st.write("This app is to increase productivity")
+st.title("My To-Do App.")
+st.subheader("This is my todoapp.")
+st.write("This app is to increase productivity.")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a To-Do", placeholder="Add the new To-Do")
+st.text_input(label="Enter a To-Do.", placeholder="Add the new To-Do.",
+              on_change=add_todo, key='new_todo')
+
+print('hello')
+
 
