bd7e6c74299b1d195448ca4ce3cd8413ccb64987
INDRANEEL8055 <codelearning8055@gmail.com>
2024-02-05 10:19:37 +0530

    created coloumns for my app titles and added into the website

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -11,5 +12,23 @@ with col2:
     content = """ Hi , I am a python developer, 
      i graduated in 2020,
      i worked at Infosys with 2 years of experience
-     WELCOME to my Portfolio   """
+     on Python,sql,oracle\n\nWELCOME to my Portfolio   """
     st.info(content)
+
+content2 = """ Below you can find 
+               some of the projects that
+                i worked upon....."""
+
+st.write(content2)
+
+col3, col4 = st.columns(2)  # dividing the my projects in two coloumns
+
+df = pandas.read_csv("data.csv",sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
