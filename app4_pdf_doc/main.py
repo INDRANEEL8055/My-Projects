fda6b1a5959d9c18880a5541a63f189471dbf1a5
INDRANEEL8055 <codelearning8055@gmail.com>
2024-02-06 09:08:52 +0530

    DAY 24 we added footer

--- app4_pdf_doc/main.py
+++ app4_pdf_doc/main.py
@@ -1,19 +1,33 @@
 from fpdf import FPDF
 import pandas as pd
 
-pdf = FPDF(orientation="P", unit="mm",format="A4")  # p is portrait.
+pdf = FPDF(orientation="P", unit="mm", format="A4")  # p is portrait.
+pdf.set_auto_page_break(auto=False, margin=0)  # pages should not be broken automatically
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
+    # sets the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)  # to set text color to RGB(makes it grey)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
     pdf.line(10, 21, 200, 21)  # for the underline below headers
 
+    # to set footer
+    pdf.ln(268)  # size of break-line is 278mm
+    pdf.set_font(family="Times", style="I", size=10)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align='R')
+
     for i in range(row["Pages"]-1):
         pdf.add_page()
+        # to set footer
+        pdf.ln(278)  # size of break-line is 278mm
+        pdf.set_font(family="Times", style="I", size=10)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align='R')
 
 
 pdf.output("output.pdf")
@@ -22,6 +36,6 @@ pdf.output("output.pdf")
 # pdf.cell(w=0,  # length of cell , x-axis
 #          h=12,  # width of cell , y-axis
 #          txt="Hello There!",  # it's the text to be added inside the cell
-#         align="L",  # align text L= left C= center
-#         ln=1,  # it's break-line if its 1 ,nxt cell will go to next line, if 0 next cell wl b added after width(w) ends
-#         border=1)  # if border = 0 then there will be no cell borders visible
+#          align="L",  # align text L= left C= center
+#          ln=1,  # it's break-line if its 1 ,nxt cell will go to next line, if 0 next cell wl b added after width(w) ends
+#          border=1)  # if border = 0 then there will be no cell borders visible
