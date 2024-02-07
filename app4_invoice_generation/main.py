e2547745027286bf1136db0cb1a112f21df7be5e
INDRANEEL8055 <codelearning8055@gmail.com>
2024-02-07 16:42:23 +0530

    Day 25:- (app4-invoice-generation) created a minimal pdf file for each excel with the help of pathlib and split

--- app4_invoice_generation/main.py
+++ app4_invoice_generation/main.py
@@ -1,10 +1,20 @@
 import pandas as pd
-import pyarrow # always import this when you are using pandas to import multiple files
+import pyarrow  # always import this when you are using pandas to import multiple files
 import openpyxl  # required by pandas to open Excel files
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="a4")
+    pdf.add_page()
+
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
