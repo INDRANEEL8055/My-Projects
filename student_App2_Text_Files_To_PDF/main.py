3ee242df9071e5eed71e377888187795a3894d77
INDRANEEL8055 <codelearning8055@gmail.com>
2024-02-12 11:01:35 +0530

    day 29 initial commmit for weather api data

--- student_App2_Text_Files_To_PDF/main.py
+++ student_App2_Text_Files_To_PDF/main.py
@@ -24,5 +24,13 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B",)
     pdf.cell(w=50, h=8, txt=name, ln=1)
 
+    # get the content of each text file
+    with open(filepath,"r") as file:
+        content = file.read()
+
+    # add the text file content to pdf
+    pdf.set_font(family='Times', size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
 # Produce the PDF
 pdf.output("output.pdf")
