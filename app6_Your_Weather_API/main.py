7ebd9a6c33c2ce7dafd870be6959c81c3a3eb79f
INDRANEEL8055 <codelearning8055@gmail.com>
2024-02-14 08:56:47 +0530

    day 31:- we also added feature to view yearly data

--- app6_Your_Weather_API/main.py
+++ app6_Your_Weather_API/main.py
@@ -12,12 +12,15 @@ app = Flask(__name__)
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]]
 
+
 @app.route("/")
 def home():
-    return render_template("home.html", data=stations.to_html())  # to render html document whenever user visits website url
+    return render_template("home.html",
+                           data=stations.to_html())  # to render html document whenever user visits website url
     # "/home"
     # also we use .to_html() to present the data on webpage as rows and columns/ table ,
     # for that we also need to add <p>{{data|safe}}</p> instead of just <p>{{data}}</p> in body of html code
+
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
@@ -30,6 +33,24 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}  # to render html document whenever user visits website url "/home"
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")  # orient = records is used to reformat the data as date, tg, etc in one pair
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)  # we removed the parse_date to change is type back to norma from date time type
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 
 
 # but for that we need that html file in templates folder
